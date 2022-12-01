8e6f07c41a2c21e6334391141a16ca7f7a44455c
Dahan13 <corentincaugant2001@gmail.com>
2022-12-01 09:37:41 +0100

    Adds tests and parameters to the database connection

--- routes/database.js
+++ routes/database.js
@@ -9,7 +9,6 @@ const params = {
     port : 3306
 }
 
-console.log("Gonna wait")
 waitPort(params)
     .then(({ open, ipVersion }) => {
         if (open) {
@@ -23,9 +22,32 @@ waitPort(params)
                 database : process.env.MYSQL_DB
             });    
 
-            // Exporting our database connection pool for later use.
-            console.log("Database connection established & exported !")
-            module.exports = pool;
+            /* This create a connection from the pool to ensure that everything is working correctly*/
+            pool.getConnection(function(err, connection) {
+                if (err) {
+                    console.error('error creating a test connection: ' + err.stack);
+                    console.log('\x1b[31m%s\x1b[0m' , 'You can find database connection settings in /routes/databaseConnector.js')
+                    return;
+                }
+            connection.release();
+            });
+
+            /* Some functions that monitors connection states & give feedbacks in dev console */
+            pool.on('acquire', function (connection) {
+                console.log('\x1b[32m%s\x1b[0m', 'Pool connection ' + connection.threadId + ' acquired');
+            });
+
+            pool.on('release', function (connection) {
+                console.log('\x1b[32m%s\x1b[0m', 'Pool connection ' + connection.threadId + ' released');
+            });
+
+            pool.on('enqueue', function () {
+                console.log('\x1b[31m%s\x1b[0m', 'A pool connection has been enqueued. Please ensure there is enough connections available for all users (default 10).\nIn case pool connections are stuck open, you need to restart the server');
+            });
+
+            module.exports = pool; // Exporting DB pool connection to all others docs.
+
+            setTimeout(() => {console.log('\x1b[33m%s\x1b[0m' ,'\n\n>> Database connection pool has been tested & exported !\n\n ========== \n')}, 1000)
         }
         else console.log('The port did not open before the timeout...');
     })
