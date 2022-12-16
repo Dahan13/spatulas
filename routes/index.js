21949338e5134325961c6945b1a346a4708721b6
Dahan13 <corentincaugant2001@gmail.com>
2022-12-16 22:13:27 +0100

    Optimizes home picture

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const registrationOpen = true;
+  const registrationOpen = false;
   res.render('home', { title: 'Home', registrationOpen: registrationOpen });
 });
 
